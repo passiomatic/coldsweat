061541b82c6011415ee7b624fc84b4dd96f83f35
Andrea Peltrin <no@spam.com>
2013-06-23 14:30:05 +0200

    Added TEMPLATE_DIR var.

--- coldsweat/app.py
+++ coldsweat/app.py
@@ -34,8 +34,7 @@ HTTP_FORBIDDEN = '403 Forbidden'
 HTTP_UNAUTHORIZED = '401 Unauthorized'
 HTTP_BAD_REQUEST = '400 Bad Request'
 
-#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
- 
+TEMPLATE_DIR = os.path.join(installation_dir, 'coldsweat/templates')
  
 # ------------------------------------------------------
 # Response
@@ -159,7 +158,7 @@ def get_template(filename, from_template):
     """
     Load an inherited template
     """
-    return tempita.Template.from_filename(os.path.join(installation_dir, 'coldsweat/templates', filename))
+    return tempita.Template.from_filename(os.path.join(TEMPLATE_DIR, filename))
         
 def fill_template(source, **kwargs):    
     t = tempita.Template(source, get_template=get_template)    
@@ -167,7 +166,7 @@ def fill_template(source, **kwargs):
 
 
 def load_template(filename):            
-    return tempita.Template.from_filename(os.path.join(installation_dir, 'coldsweat/templates', filename), get_template=get_template)
+    return tempita.Template.from_filename(os.path.join(TEMPLATE_DIR, filename), get_template=get_template)
 
 
 def make_page(filename, filler, symbols):

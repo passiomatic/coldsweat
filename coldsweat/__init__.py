fa5717cbd2f547f71d8274367e82095aa3663db0
Andrea Peltrin <no@spam.com>
2013-07-02 21:43:16 +0200

    Removed more obsolete code.

--- coldsweat/__init__.py
+++ coldsweat/__init__.py
@@ -22,26 +22,6 @@ DEFAULT_USER_AGENT = 'Coldsweat/%s Feed Fetcher <http://lab.passiomatic.com/cold
 #  to work for the fetcher script too
 installation_dir, _ = path.split(path.dirname(path.abspath(__file__)))
 
-# def load_config(filename):
-#     
-#     # Defaults to bootstrap system
-#     config = SafeConfigParser(defaults={
-#         'engine': 'sqlite', 
-#         'filename': path.join(installation_dir, 'data/coldsweat.db'), 
-#         'error_threshold': 100,
-#         'user_agent': DEFAULT_USER_AGENT,
-#         'min_interval': 60*15,
-#         'max_history': 7,        
-#         'filename': path.join(installation_dir, 'coldsweat.log'),
-#         'level': 'DEBUG',
-#         'format': '%(asctime)s %(levelname)s: %(message)s',
-#         'datefmt': '%Y-%m-%d %H:%M:%S',
-#     })
-# 
-#     config.read(filename)    
-#     return config
-#config = load_config(path.join(installation_dir, 'etc/config'))
-
 # Set up configuration settings
 config = RawConfigParser()
 #@@TODO: Check environ var first for config path

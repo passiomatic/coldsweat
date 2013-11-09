5e1d37e904009a0e32e160e19536cc81e060c319
Andrea Peltrin <no@spam.com>
2013-11-09 15:07:08 +0100

    Added app entry point for WSGI servers (Issue #32).

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+Description: entry point for WSGI environments
+
+Copyright (c) 2013— Andrea Peltrin
+License: MIT (see LICENSE.md for details)
+'''
+
+from coldsweat.app import setup_app
+app = setup_app()

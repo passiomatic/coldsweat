03fc844d96a4a21be51bf10ab7d3a16b580a441a
Andrea Peltrin <andrea@passiomatic.com>
2014-07-26 11:25:38 +0200

    Fixes #71.

--- coldsweat/frontend.py
+++ coldsweat/frontend.py
@@ -554,9 +554,9 @@ def get_feed_entries(user, feed, *select):
 
 def get_feeds(user, *select):  
     select = select or [Feed, Icon, fn.Count(Entry.id).alias('entries')]
-    q = Feed.select(*select).join(Icon).switch(Feed).join(Entry, JOIN_LEFT_OUTER).switch(Feed).join(Subscription).where(Subscription.user == user).group_by(Feed)
-
-    return q    
+    q = Feed.select(*select).join(Icon).switch(Feed).join(Entry, JOIN_LEFT_OUTER).switch(Feed).join(Subscription).where(Subscription.user == user).group_by(Feed, Icon)
+    
+    return q  
 
 # Groups
 

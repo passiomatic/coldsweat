3768ef7b5cfd74482b009b4eec879a3f4a443561
Oz Tiram <oz.tiram@noris.de>
2019-08-16 15:22:02 +0200

    Pep8ify

--- coldsweat/__init__.py
+++ coldsweat/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-Coldsweat - RSS aggregator and web reader compatible with the Fever API 
+Coldsweat - RSS aggregator and web reader compatible with the Fever API
 
 Copyright (c) 2013—2016 Andrea Peltrin
 Portions are copyright (c) 2013 Rui Carmo
@@ -13,7 +13,7 @@ __license__ = 'MIT'
 
 import os
 import logging
-from config import *
+from config import load_config
 
 # Define an informal API for plugin implementations
 
@@ -21,31 +21,32 @@ __all__ = [
     'VERSION_STRING',
     'USER_AGENT',
 
-    # Synthesized entries and feed URI's 
+    # Synthesized entries and feed URI's
     'FEED_TAG_URI',
     'ENTRY_TAG_URI',
-    
+
     # Configuration
     'installation_dir',
     'template_dir',
     'plugin_dir',
     'config',
-    
+
     # Logging
     'logger',
 ]
 
-VERSION_STRING  = '%d.%d.%d%s' % __version__
-USER_AGENT      = 'Coldsweat/%s Feed Fetcher <http://lab.passiomatic.com/coldsweat/>' % VERSION_STRING
+VERSION_STRING = '%d.%d.%d%s' % __version__
+USER_AGENT = ('Coldsweat/%s Feed Fetcher <http://lab.passiomatic.com/'
+              'coldsweat/>' % VERSION_STRING)
 
-FEED_TAG_URI    = 'tag:lab.passiomatic.com,2017:coldsweat:feed:%s'         
-ENTRY_TAG_URI   = 'tag:lab.passiomatic.com,2017:coldsweat:entry:%s'
+FEED_TAG_URI = 'tag:lab.passiomatic.com,2017:coldsweat:feed:%s'
+ENTRY_TAG_URI = 'tag:lab.passiomatic.com,2017:coldsweat:entry:%s'
 
-# Figure out installation directory. This has 
+# Figure out installation directory. This has
 #  to work for the fetcher script too
-installation_dir, _ = os.path.split(os.path.dirname(os.path.abspath(__file__))) 
-template_dir        = os.path.join(installation_dir, 'coldsweat/templates')
-plugin_dir          = os.path.join(installation_dir, 'plugins')
+installation_dir, _ = os.path.split(os.path.dirname(os.path.abspath(__file__)))
+template_dir = os.path.join(installation_dir, 'coldsweat/templates')
+plugin_dir = os.path.join(installation_dir, 'plugins')
 
 # ------------------------------------------------------
 # Load up configuration settings
@@ -62,14 +63,12 @@ logger = logging.getLogger()
 
 if config.log.filename:
     logging.basicConfig(
-        filename    = config.log.filename,
-        level       = getattr(logging, config.log.level),
-        format      = '[%(asctime)s] %(process)d %(levelname)s %(message)s',
+        filename=config.log.filename,
+        level=getattr(logging, config.log.level),
+        format='[%(asctime)s] %(process)d %(levelname)s %(message)s',
     )
-    for module in 'peewee', 'requests':        
+    for module in 'peewee', 'requests':
         logging.getLogger(module).setLevel(logging.WARN)
 else:
-    # Silence is golden 
+    # Silence is golden
     logger.addHandler(logging.NullHandler())
-
-

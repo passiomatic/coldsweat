79d4c07bcb4ee7178fe9a58b644758fac63e5fc2
Oz N Tiram <oz.tiram@gmail.com>
2019-08-27 23:28:08 +0200

    pep8ify test_fever

--- tests/test_fever.py
+++ tests/test_fever.py
@@ -6,14 +6,18 @@ Description: quick & dirty test suite for Fever API implementation
 Copyright (c) 2013—2016 Andrea Peltrin
 License: MIT (see LICENSE for details)
 '''
-import sys, subprocess, optparse
+import optparse
+import subprocess
+
 from datetime import datetime
 
-from ..utilities import datetime_as_epoch
-from ..models import User
+from coldsweat.utilities import datetime_as_epoch
+from coldsweat.models import User
 
 TEST_USER_CREDENTIALS = 'test@example.com', 'coldsweat'
-ALL = 'groups feeds unread_item_ids saved_item_ids favicons items links unread_recently_read mark_item mark_feed mark_group mark_all'.split()
+ALL = ('groups feeds unread_item_ids saved_item_ids favicons items links '
+       'unread_recently_read mark_item mark_feed mark_group mark_all').split()
+
 
 def run_tests(endpoint, suites=ALL):
     """
@@ -23,7 +27,7 @@ def run_tests(endpoint, suites=ALL):
     epoch = datetime_as_epoch(datetime.utcnow())
 
     queries = []
-    
+
     if 'groups' in suites:
         queries.extend([
             (False, 'groups')
@@ -51,93 +55,100 @@ def run_tests(endpoint, suites=ALL):
 
     if 'items' in suites:
         queries.extend([
-            (False, 'items'),         
-            (False, 'items&with_ids=50,51,52'),         
+            (False, 'items'),
+            (False, 'items&with_ids=50,51,52'),
             (False, 'items&max_id=5'),
             (False, 'items&since_id=50')
         ])
 
     if 'links' in suites:
         queries.extend([
-            (False, 'links')                                        # Unsupported
-        ])          
+            (False, 'links')                                 # Unsupported
+        ])
 
     if 'mark_item' in suites:
         queries.extend([
-            (True, 'mark=item&as=read&id=1'), 
-            (True, 'mark=item&as=read&id=1'),                       # Dupe
-            (True, 'mark=item&as=read&id=0'),                       # Does not exist
-            (True, 'mark=item&as=unread&id=1'), 
-            (True, 'mark=item&as=saved&id=1'), 
-            (True, 'mark=item&as=saved&id=1'),                      # Dupe
-            (True, 'mark=item&as=saved&id=0'),                      # Does not exist
-            (True, 'mark=item&as=unsaved&id=1'), 
-        ]) 
-        
+            (True, 'mark=item&as=read&id=1'),
+            (True, 'mark=item&as=read&id=1'),                # Dupe
+            (True, 'mark=item&as=read&id=0'),                # Does not exist
+            (True, 'mark=item&as=unread&id=1'),
+            (True, 'mark=item&as=saved&id=1'),
+            (True, 'mark=item&as=saved&id=1'),               # Dupe
+            (True, 'mark=item&as=saved&id=0'),               # Does not exist
+            (True, 'mark=item&as=unsaved&id=1'),
+        ])
+
     if 'mark_feed' in suites:
         queries.extend([
-            (True, 'mark=feed&as=read&id=1&before=%d' % epoch), 
-            (True, 'mark=feed&as=read&id=1&before=abc'),            # Malformed
-            (True, 'mark=blah&as=read&id=1&before=%d' % epoch),     # Malformed (3)
-            (True, 'mark=feed&as=read&id=foo&before=abc'),          # Malformed (4)
-            (True, 'mark=feed&as_=read&id=1&before=%d' % epoch),    # Malformed (5)
-            (True, 'mark=feed&as=read&id=0&before=%d' % epoch),     # Does not exist
-        ]) 
+            (True, 'mark=feed&as=read&id=1&before=%d' % epoch),
+            (True, 'mark=feed&as=read&id=1&before=abc'),         # Malformed
+            (True, 'mark=blah&as=read&id=1&before=%d'
+             % epoch),  # Malformed (3)
+            (True, 'mark=feed&as=read&id=foo&before=abc'),     # Malformed (4)
+            (True, 'mark=feed&as_=read&id=1&before=%d'
+             % epoch),    # Malformed (5)
+            (True, 'mark=feed&as=read&id=0&before=%d'
+             % epoch),  # Does not exist
+        ])
 
     if 'mark_group' in suites:
         queries.extend([
-            (True, 'mark=group&as=read&id=1&before=%d' % epoch), 
-            (True, 'mark=group&as=read&id=1&before=abc'),           # Malformed 
-            (True, 'mark=group&as=read&id=-1&before=%d' % epoch),   # Unsupported
-        ]) 
+            (True, 'mark=group&as=read&id=1&before=%d' % epoch),
+            (True, 'mark=group&as=read&id=1&before=abc'),     # Malformed
+            (True, 'mark=group&as=read&id=-1&before=%d'
+             % epoch),   # Unsupported
+        ])
 
     if 'mark_all' in suites:
         queries.extend([
-            (True, 'mark=group&as=read&id=0&before=%d' % epoch),    # Mark all as read 
-        ])         
+            (True, 'mark=group&as=read&id=0&before=%d'
+             % epoch),  # Mark all as read
+        ])
 
     if 'unread_read' in suites:
         queries.extend([
             (True, 'unread_recently_read=1')
-        ])          
-                              
+        ])
+
     # Test auth failure
-    print ('\n= auth (failure)\n')
+    print('\n= auth (failure)\n')
 
     subprocess.call([
-        "curl", 
+        "curl",
         "-dapi_key=%s" % 'wrong-api-key',
         "%s?api&unread_item_ids" % endpoint
     ])
-    
-    api_key=User.make_api_key(*TEST_USER_CREDENTIALS)
 
-    # Test API commands            
+    api_key = User.make_api_key(*TEST_USER_CREDENTIALS)
+
+    # Test API commands
     for as_form, q in queries:
-        print ('\n= %s\n' % q)
+        print('\n= %s\n' % q)
 
         if as_form:
             subprocess.call([
-                "curl", 
+                "curl",
                 "-dapi_key=%s" % api_key,
                 "-d%s" % q,
                 "%s?api" % endpoint
             ])
         else:
             subprocess.call([
-                "curl", 
+                "curl",
                 "-dapi_key=%s" % api_key,
                 "%s?api&%s" % (endpoint, q),
             ])
-            
+
 
 parser = optparse.OptionParser(
     usage='%prog endpoint [-s suite]'
     )
 parser.add_option(
     '-s', '--suite',
-    dest='suite', 
-    help='the Fever API test suite to run. Available suites are: groups, feeds, unread, saved, favicons, items, links, unread_read, mark_item, mark_feed mark_group, mark_all')
+    dest='suite',
+    help=('the Fever API test suite to run. Available suites are: groups,'
+          'feeds,unread, saved, favicons, items, links, unread_read,'
+          'mark_item,mark_feed mark_group, mark_all'))
 
 
 if __name__ == '__main__':
@@ -145,11 +156,12 @@ if __name__ == '__main__':
     options, args = parser.parse_args()
 
     if not args:
-        parser.error("no Fever API endpoint given, e.g.: http://localhost:8080/fever/")
+        parser.error("no Fever API endpoint given, e.g.:"
+                     "http://localhost:8080/fever/")
     elif len(args) > 1:
-        parser.error("extraneous argument found, use -s suite to run a specific test suite")
-        
-    suite = options.suite if options.suite else ALL
-    
-    run_tests(args[0], suite)
+        parser.error("extraneous argument found, use -s suite to run a "
+                     "specific test suite")
 
+    suite = options.suite if options.suite else ALL
+
+    run_tests(args[0], suite)

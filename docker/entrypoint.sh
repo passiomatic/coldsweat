db2b6255da5246a9c7277ac231b471960d241828
Oz N Tiram <oz.tiram@gmail.com>
2019-08-31 23:15:50 +0200

    Fix serving of static files

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 UWSGI_PORT="${UWSGI_PORT:-9001}"
-UWSGI_PROCS=${UWSGI_PROCS:-1}
-UWSGI_THREADS=${UWSGI_THREADS:-1}
+UWSGI_PROCS=${UWSGI_PROCS:-4}
+UWSGI_THREADS=${UWSGI_THREADS:-4}
 
 export COLDSWEAT_INSTALL_DIR=/run/coldsweat
 export COLDSWEAT_CONFIG_PATH=/etc/coldsweat/config
@@ -18,7 +18,10 @@ exec uwsgi --need-app --plugins-dir /usr/lib/uwsgi  --need-plugin python3 \
 	--pythonpath /run/coldsweat \
 	--plugin python3 --processes "${UWSGI_PROCS}" \
 	--threads "${UWSGI_THREADS}" --http-socket 0.0.0.0:"${UWSGI_PORT}" \
-	--static-map /static=/var/lib/coldsweat/static \
+	--static-map /static=/run/coldsweat/static/ \
+	--static-map /stylesheets=/run/coldsweat/static/stylesheets \
+	--static-map /javascripts/=/run/coldsweat/static/javascripts \
+	--static-map /fonts/=/run/coldsweat/static/fonts \
 	--wsgi-file=/run/coldsweat/wsgi.py --callable=app --master \
 	--log-master \
 	--logformat 'pid: %(pid)|app: -|req: -/-] %(var.HTTP_X_REAL_IP) (%(user)) {%(vars) vars in %(pktsize) bytes} [%(ctime)] %(method) %(uri) => generated %(rsize) bytes in %(msecs) msecs (%(proto) %(status)) %(headers) headers in %(hsize) bytes (%(switches) switches on core %(core))'

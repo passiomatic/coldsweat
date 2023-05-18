c5a2d68f0388a6c711d804fb392dfc4dd1b075a5
passiomatic <andrea@passiomatic.com>
2023-05-18 19:18:49 +0300

    Add Flask Login PoC

--- coldsweat/app2.py
+++ coldsweat/app2.py
@@ -3,10 +3,12 @@ from datetime import datetime, date, timezone, timedelta
 # from operator import attrgetter
 # from itertools import groupby, islice
 import flask
+import flask_login
 from peewee import *
 import coldsweat.models as models
 
 app = flask.Flask(__name__)
+app.secret_key = 'super secret string'  # Change this!
 
 
 @app.before_request
@@ -39,6 +41,75 @@ def human_date(value):
 #         "last_sync": models.get_last_log(),
 #         "nav_tags": get_nav_tags(MAX_TOP_TAGS)
 #     }
+
+
+# ---------
+# User auth
+# ---------
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+
+class SessionUser(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if not models.User.get_or_none(email=email):
+        return
+
+    user = SessionUser()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if not models.User.get_or_none(email=email):
+        return
+
+    user = SessionUser()
+    user.id = email
+    return user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='email' name='email' id='email' value="andrea@passiomatic.com" placeholder='email'/>
+                <input type='text' name='password' value="password" id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+    email = flask.request.form['email']
+    password = flask.request.form['password']
+    user = models.User.validate_credentials(email, password)
+    if user:
+        session_user = SessionUser()
+        session_user.id = email
+        flask_login.login_user(session_user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return 'Bad login'
+
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
 
 # ---------
 # Home

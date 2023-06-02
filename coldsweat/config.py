880bbb9bb6bdd5c97c384ed0b9990cb76f67fa39
passiomatic <andrea@passiomatic.com>
2023-06-02 16:46:11 +0300

    Use Flask instance folder

--- coldsweat/config.py
+++ coldsweat/config.py
@@ -11,4 +11,4 @@ base_dir = Path(__file__).parent.parent
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or "some secret key"
     DATABASE_URL = os.environ.get('DATABASE_URL')\
-        or f"sqlite:///{base_dir.joinpath('data', 'coldsweat.db')}"
+        or f"sqlite:///{base_dir.joinpath('instance', 'coldsweat.db')}"

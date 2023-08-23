89889bd770847aefce25f5ded412345489bfe04b
passiomatic <andrea@passiomatic.com>
2023-08-23 17:55:15 +0200

    Show disabled feeds as line-through text

--- coldsweat/main/queries.py
+++ coldsweat/main/queries.py
@@ -7,7 +7,7 @@ from ..models import (Entry, Feed, Group, Read, Saved, Subscription)
 
 
 def get_groups_and_feeds(user):
-    q = (Feed.select(Feed.id, Feed.title, Feed.icon_url, Group.id.alias('group_id'), Group.title.alias('group_title'))
+    q = (Feed.select(Feed.id, Feed.enabled, Feed.title, Feed.icon_url, Group.id.alias('group_id'), Group.title.alias('group_title'))
         .join(Subscription)
         .join(Group)
         .where((

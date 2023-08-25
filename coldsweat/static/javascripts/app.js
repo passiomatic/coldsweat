7c956eb7dcb1a8e280086c26c3182a4ae556f909
passiomatic <andrea@passiomatic.com>
2023-08-25 16:30:56 +0200

    Reorganize files a bit

--- coldsweat/static/javascripts/app.js
+++ coldsweat/static/javascripts/app.js
@@ -1,201 +1,242 @@
-$(document).ready(function() {
-    "use strict";
+window.addEventListener("DOMContentLoaded", (event) => {
 
-    var flash_fragment = $('<div class="flash"><i class="fa fa-3x"></i><div class="message">&nbsp;</div></div>')
-    var alert_fragment = $('<div class="alert alert--error" style="display:none"></div>')
-    var modal_fragment = $('<div role="dialog" class="modal fade hide"></div>')
-    var loading_fragment = $('<div class="loading"><i class="fa fa-spinner fa-spin"></i> Loading&hellip;</div>')
-    //var loading_favicon_fragment = $('<i class="favicon fa fa-spinner fa-spin"></i>')
+    // function findParent(currentNode, parentClass) {
+    //     while (currentNode.tagName != 'BODY') {
+    //         if (currentNode.classList.contains(parentClass)) {
+    //             return currentNode;
+    //         }
+    //         currentNode = currentNode.parentNode;
+    //     }
+    //     return null;
+    // }
 
-    $(document).ajaxError(function(event, jqXHR, settings, exception) {
-        alert('Oops! An error occurred while processing your request: ' + exception)
-    });
-
-    function flash(icon, message) {
-        var fragment = flash_fragment.clone()
-        fragment.find('i').addClass('fa-' + icon)
-        fragment.find('.message').text(message)
-        $(document.body).append(fragment);
-        fragment.show().animate({'opacity': 'hide'}, 'slow', function() { fragment.remove() } )         
-    }
-    
-    function alert(message) {
-        alert_fragment.text(message)
-        $(document.body).prepend(alert_fragment);
-        alert_fragment.slideDown('fast')
-    }    
-
-    function endpoint(pathname) {
+    function makeEndpointURL(pathname) {
         var segments = [
             window.applicationURL,
             pathname
         ]
-                          
         return segments.join('')
     }
-    
-    function mark(id, status) {
-        $.ajax(endpoint('/entries/') + id, {
-            type: 'POST', 
-            data: 'mark&as=' + status
-        })        
-    }    
 
-    function toggleRead(event) {
-        var icon = $(event.target)
-        var li = icon.parents('li').first()
-        li.toggleClass('status-read');                
-        var status = 'read'
-        if(li.hasClass('status-read')) {
-            //if(event.type != 'click') { flash('circle-o', 'Read'); }
-            flash('circle-o', 'Read');
-            icon.removeClass('icon-unread').addClass('icon-read')                            
-        }else{
-            status = 'unread'  
-            flash('circle', 'Unread');
-            icon.removeClass('icon-read').addClass('icon-unread')                
-        } 
-        mark(li.data('entry'), status)
-    }
+    // Innermost element that does not get replaced
+    var listViewEl = document.getElementById("panel");
 
-    function toggleSaved(event) {
-        var icon = $(event.target)
-        var li = icon.parents('li').first()
-        li.toggleClass('status-saved')                
-        var status = 'saved'
-        if(li.hasClass('status-saved')) {
-            flash('star', 'Saved');
-            icon.removeClass('icon-unsaved').addClass('icon-saved')                            
-        } else {
-            status = 'unsaved'
-            flash('star-o', 'Unsaved');
-            icon.removeClass('icon-saved').addClass('icon-unsaved')                            
-        }
-        mark(li.data('entry'),  status);
-    }
+    const Sweat = {
 
-    function open(pathname) {       
-        return function() {
-            window.location.assign(endpoint(pathname))            
-        } 
-    }
-    
-    function moveTo(direction) {        
-        return function (event) {            
-            // Each() deals with empty set too
-            $(direction == 'next' ? '.view a[rel=next]' : '.view a[rel=prev]').each(function(){                
-                window.location.assign(($(this).attr('href')))
-            })
-        }
-    }
 
-    function bindKeyboardShortcuts() {
-        var events = {
-            '1': open('entries?filter=unread'),
-            '2': open('entries?filter=saved'),
-            '3': open('entries?filter=all'),
-            '4': open('groups'),
-            '5': open('feeds'), 
-            //'a': function() { $('nav .add-trigger').click() },
-            'm': function() { $('.entry.expanded .read-trigger').click() },
-            's': function() { $('.entry.expanded .save-trigger').click() },
-            //'j': moveTo('prev'),
-            'k': moveTo('next'),
-            // Bypass pop-up blocking mechanism (unfortunately a referrer URL is sent)
-            'v': function(event) { event.preventDefault(); $('.entry.expanded .content a.btn-visit').each(function(){ window.open($(this).attr('href'),'_blank') }) }
-        }
-    
-        for (var key in events) {      
-            $(document).bind('keypress', key, events[key])  
-        }
-    }
-    
-    function setup() {               
-        bindKeyboardShortcuts();
-
-        // Setup popovers content using HTML
-        //   from a given DOM element
-        $('.popover-trigger').one('click', function(event) {
-            event.preventDefault()
-            var $this = $(this)
-            var template = $this.attr('href')
-            $this.popover({
-                html: true, 
-                placement: 'bottom', 
-                content: $(template).html()
-            })               
-            $this.popover('show')
-        }).one('shown', function(event) {
-            var $this = $(this)
-            var $popover = $this.data('popover').$tip
-            $popover.delegate('a', 'click', function (event) {
-                $this.popover('hide');
+        setup: function () {
+            window.addEventListener("popstate", (event) => {
+                //console.log('popstate', event.state)
             });
-        })
-        
-        // Setup tooltips with global options
-        $('a[data-toggle="tooltip"]').tooltip({delay: 500})
-        
-        // Remote modals
-        $(document).on('click', '[data-remote-modal]', function(event) { 
-            event.preventDefault()
-            var fragment = modal_fragment.clone() 
-            fragment.attr('id', $(this).data('remote-modal'))
-            fragment.load($(this).attr('href'), function(response, status, jqXHR) {
-                fragment.html(response)
-                fragment.on('hidden', function(event) {
-                    fragment.remove() // Remove from DOM
-                })
-                fragment.modal('show')
-            });            
-        })
-      
-        $(document).on('submit', '.modal form[data-ajax-post]', function(event) { 
-            event.preventDefault()
-            var form = $(event.target)
-            var serializedData = form.serialize()
-            form.find('.modal-footer').html(loading_fragment)
-            $.ajax(form.attr('action'), { 
-                type: form.attr('method'), 
-                data: serializedData}).done(function(data, textStatus, jqXHR) {                               
-                    // Replace form with response only if HTML (it could be script)
-                    var contentType = jqXHR.getResponseHeader('content-type') || '';
-                    if (contentType.indexOf('text/html') == 0) {
-                        form.replaceWith(data)                   
-                    }
-            })       
-        })
 
-        // 'More...' link
-        $(document).on('click', '.panel li.more a', function(event) { 
-            event.preventDefault()            
-            var parent = $(event.target).parents('li.more').first()
-            parent.html(loading_fragment)
-            $.ajax($(this).attr('href'), 
-                {dataType: 'html', type:'GET'}).done(function(data) {            
-                   $(document.body).animate({'scrollTop': parent.position().top}, 500)
-                   parent.replaceWith(data)
-            })       
-        })
+            // Prev
+            hotkeys('j', function (event, handler) {
+                var currentCardInput = listViewEl.querySelector(".entry .entry-card input:checked");
+                var prevCard = null;
+                var prevCardInput = null;
+                if (currentCardInput) {
+                    var li = currentCardInput.parentNode.parentNode;
+                    // @@TODO Skip headings
+                    prevCard = li.previousElementSibling;
+                    prevCardInput = prevCard.querySelector(".entry-card input");
+                } else {
+                    // Select first
+                    prevCardInput = listViewEl.querySelector(".entry .entry-card input");
+                }
+                if (prevCardInput) {
+                    prevCardInput.checked = true;
+                    Sweat.mark(prevCardInput.value, 'read');
+                    Sweat.loadEntry(prevCardInput.value, event);
+                }
+            });
+            // Next
+            hotkeys('k', function (event, handler) {
+                var currentCardInput = listViewEl.querySelector(".entry .entry-card input:checked");
+                var nextCard = null;
+                var nextCardInput = null;
+                if (currentCardInput) {
+                    var li = currentCardInput.parentNode.parentNode;
+                    // @@TODO Skip headings
+                    nextCard = li.nextElementSibling;
+                    nextCardInput = nextCard.querySelector(".entry-card input");
+                } else {
+                    // Select first
+                    nextCardInput = listViewEl.querySelector(".entry .entry-card input");
+                }
+                if (nextCardInput) {
+                    nextCardInput.checked = true;
+                    Sweat.mark(nextCardInput.value, 'read')
+                    Sweat.loadEntry(nextCardInput.value, event)
+                }
+            });
+        },
 
-        // Mark entry as read, saved etc.
-        $(document).on('click', '.view .read-trigger', function(event) { toggleRead(event) })
-        $(document).on('click', '.view .save-trigger', function(event) { toggleSaved(event) })
+        submitRemoteForm: function (url, event) {
+            var dialogEl = document.getElementById('dialog');
 
-        // Profile form 
-        $(document).on('click', '#show-password-trigger', function(event) {
-            event.preventDefault()
-            var $password = $('#password_')
-            if ($password.prop('type') == 'password') {
-                $(this).text('Hide')
-                $password.prop('type', 'text')    
-            } else {
-                $(this).text('Show')
-                $password.prop('type', 'password')
+            // @@TODO Add spinner
+            var button = dialogEl.querySelector('button[type=submit]');
+            if (button) {
+                button.disabled = true;
             }
-        })
+
+            event.preventDefault();
+            var formData = new FormData(event.target);
+            fetch(makeEndpointURL(url), { method: 'POST', body: formData })
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(`Server returned error ${response.status} while handing POST request`);
+                    }
+                    response.text().then((text) => {
+                        const contentType = response.headers.get("content-type");
+                        //console.log("contentType: " + contentType)
+                        if (contentType.startsWith('text/html')) {
+                            dialogEl.querySelector('.dialog-content').innerHTML = text;
+                        } else if (contentType.startsWith('text/javascript')) {
+                            eval(text);
+                        }
+                    });
+
+
+                });
+        },
+
+        openRemoteDialog: function (url, event) {
+            var dialogEl = document.getElementById('dialog');
+
+            fetch(url)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(`Server returned error ${response.status} while handling request ${url}`);
+                    }
+                    response.text().then((text) => {
+                        dialogEl.querySelector('.dialog-content').innerHTML = text;
+                        Sweat.openDialog(dialogEl, event)
+                    });
+                });
+        },
+
+        loadEntry: function (id, title, event) {
+            var mainEl = document.getElementById('main');
+            // Remove any previous animation triggers
+            mainEl.classList.remove('in')
+            Sweat.replaceElementWithTemplate(`template-${id}`, mainEl, event);
+            window.requestAnimationFrame((timeStamp) => {
+                // Add on next frame to trigger entering animation
+                mainEl.classList.add('in')
+            })
+
+            Sweat.mark(id, 'read');
+            //document.title = `${title} • Coldsweat`;
+            event.preventDefault();
+        },
+
+        replaceElementWithTemplate: function (templateId, targetEl, event) {
+            var templateEl = document.getElementById(templateId);
+            //var targetEl = document.getElementById(targetId);
+            var sourceElCopy = templateEl.content.firstElementChild.cloneNode(true);
+            targetEl.replaceChildren(...[sourceElCopy]);
+        },
+
+        loadFolder: function (url, title, event) {
+            if (event) {
+                event.preventDefault();
+            }
+            var inputEl = event.currentTarget.previousElementSibling
+            if (inputEl) {
+                inputEl.checked = true;
+            }
+            fetch(url)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(`Server returned error ${response.status} while handling request ${url}`);
+                    }
+                    response.text().then((text) => {
+                        var panelEl = document.getElementById('panel');
+                        panelEl.innerHTML = text;
+                        var listEl = panelEl.querySelector('.list-view')
+                        window.requestAnimationFrame((timeStamp) => {
+                            // Add on next frame to trigger entering animation 
+                            listEl.classList.add('in')
+                        }
+                        )
+                        var newUrl = new URL(url);
+                        // We want to restore the whole page
+                        newUrl.searchParams.delete('xhr');
+                        history.pushState({}, '', newUrl.href)
+                        if (title) {
+                            document.title = `${title} • Coldsweat`;
+                        }
+                    });
+                });
+        },
+
+        closeDialog: function (event) {
+            var dialog = document.getElementById('dialog');
+            //dialog.classList.remove("in"); 
+            dialog.close();
+        },
+
+        openDialog: function (dialogEl, event) {
+            dialogEl.showModal();
+            //dialogEl.classList.add("in"); 
+            return dialogEl;
+        },
+
+        markSaved: function (id, event) {
+            var toggle = event.target;
+            var new_value = toggle.checked ? 'saved' : 'unsaved'
+            Sweat.mark(id, new_value)
+        },
+
+        mark: function (id, status) {
+            var formData = new FormData();
+            formData.append('mark', '')
+            formData.append('as', status)
+            fetch(makeEndpointURL(`/entries/${id}`), { method: 'POST', body: formData })
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(`Server returned error ${response.status} while handing POST request`);
+                    }
+                    response.text().then((text) => {
+                    });
+                });
+        },
+
+        loadMore: function (url) {
+            var wrapper = document.getElementById("entry-list");
+            fetch(url)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(`Server returned error ${response.status} while handling pagination`);
+                    }
+                    response.text().then((text) => {
+                        var button = wrapper.querySelector(".more");
+                        button.remove();
+                        var template = document.createElement('template');
+                        template.innerHTML = text;
+                        template.content.childNodes.forEach((child) => {
+                            wrapper.appendChild(child.cloneNode(true));
+                        })
+                        //child.scrollIntoView({ behavior: "smooth", block: "start" })
+                    });
+                });
+        },
+
+        shareEntry: async function (shareData) {
+            if (navigator.share) {
+                try {
+                    await navigator.share(shareData);
+                } catch (err) {
+                    console.log('Error while sharing entry: ', err)
+                }
+            } else {
+                // @@TODO Copy URL to clipboard as fallback 
+            }
+        }
     }
-    
-    setup();
-    
-});
+
+    Sweat.setup();
+    window.Sweat = Sweat;
+
+})
